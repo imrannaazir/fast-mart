c3175c5d759ceb40a9e0e48d1358ad3df2c1daf8
emon <imrannaaziremon@gmail.com>
2023-08-15 23:21:34 +0600

    created api for updating signle billboard

--- app/api/[storeId]/billboards/[billboardId]/route.ts
+++ app/api/[storeId]/billboards/[billboardId]/route.ts
@@ -62,3 +62,57 @@ export async function DELETE(
     return new NextResponse("Internal error", { status: 500 });
   }
 }
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { billboardId: string; storeId: string } }
+) {
+  try {
+    const { userId } = auth();
+
+    const body = await req.json();
+    const { label, imageUrl } = body;
+
+    if (!userId) {
+      return new NextResponse("Unauthenticated", { status: 403 });
+    }
+
+    if (!params.billboardId) {
+      return new NextResponse("Billboard id is required", { status: 400 });
+    }
+
+    if (!label) {
+      return new NextResponse("Label is required", { status: 400 });
+    }
+
+    if (!imageUrl) {
+      return new NextResponse("Image Url is required", { status: 400 });
+    }
+
+    const storeByUserId = await prismaDb.store.findFirst({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+    });
+
+    if (!storeByUserId) {
+      return new NextResponse("Unauthorized", { status: 405 });
+    }
+
+    const billboard = await prismaDb.billboard.update({
+      where: {
+        id: params.billboardId,
+      },
+      data: {
+        label,
+        imageUrl,
+      },
+    });
+
+    return NextResponse.json(billboard);
+  } catch (error) {
+    console.log("[BILLBOARD_PATCH]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
