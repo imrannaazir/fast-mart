b4b47baf4bfa89dfe04fb5d2cca5840b9692ddbd
emon <imrannaaziremon@gmail.com>
2023-08-15 23:20:15 +0600

    created api for getting single billboard

---
+++ app/api/[storeId]/billboards/[billboardId]/route.ts
@@ -0,0 +1,26 @@
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+import prismaDb from "@/lib/prismadb";
+
+export async function GET(
+  req: Request,
+  { params }: { params: { billboardId: string } }
+) {
+  try {
+    if (!params.billboardId) {
+      return new NextResponse("Billboard id is required", { status: 400 });
+    }
+
+    const billboard = await prismaDb.billboard.findUnique({
+      where: {
+        id: params.billboardId,
+      },
+    });
+
+    return NextResponse.json(billboard);
+  } catch (error) {
+    console.log("[BILLBOARD_GET]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
