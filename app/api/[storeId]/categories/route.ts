52e49a9c5e0d012cff8a79a111deef5cce02190b
emon <imrannaaziremon@gmail.com>
2023-08-18 21:50:11 +0600

    created new api for gettign all categories

--- app/api/[storeId]/categories/route.ts
+++ app/api/[storeId]/categories/route.ts
@@ -49,3 +49,25 @@ export async function POST(
     return new NextResponse("Internal error", { status: 500 });
   }
 }
+
+export async function GET(
+  req: Request,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    if (!params.storeId) {
+      return new NextResponse("Store ID is required.", { status: 400 });
+    }
+
+    const categories = await prismaDb.category.findMany({
+      where: {
+        storeId: params.storeId,
+      },
+    });
+
+    return NextResponse.json(categories);
+  } catch (error) {
+    console.log("[CATEGORIES_GET", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
