81883f32e7ce5c3b62f8cbccfff545c88263165e
emon <imrannaaziremon@gmail.com>
2024-02-17 07:59:35 +0600

    deleted user controller

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,15 +1,1 @@
-import { Router } from 'express';
-import validateRequest from '../../middlewares/validateRequest';
-import { createUserValidationSchema } from './user.validation';
-import UserController from './user.controller';
-
-const router = Router();
-
-// create user : POST
-router.get(
-  '/:id',
-  // validateRequest(createUserValidationSchema),
-  UserController.createUser,
-);
-const UserRoutes = router;
-export default UserRoutes;
+ 
