58846b6eeb19cd30def864e0eb8ea2c50405648c
emon <imrannaaziremon@gmail.com>
2024-02-19 13:11:55 +0600

    [SETUPED] - redux and create auth slice

---
+++ src/redux/features/api/baseApi.ts
@@ -0,0 +1,24 @@
+import { RootState } from "@/redux/store";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: "/",
+  prepareHeaders: (headers, { getState }) => {
+    const token = (getState() as RootState).auth.token;
+
+    // If we have a token set in state, let's assume that we should be passing it.
+    if (token) {
+      headers.set("authorization", `Bearer ${token}`);
+    }
+
+    return headers;
+  },
+});
+
+const baseApi = createApi({
+  reducerPath: "api",
+  baseQuery,
+  endpoints: () => ({}),
+});
+
+export default baseApi;
