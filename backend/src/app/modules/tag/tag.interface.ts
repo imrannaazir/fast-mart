02858c81e2bd9584a7ec676caa83844ee1338329
emon <imrannaaziremon@gmail.com>
2024-04-08 16:14:49 +0600

    [MODIFIED] - tag module

--- backend/src/app/modules/tag/tag.interface.ts
+++ backend/src/app/modules/tag/tag.interface.ts
@@ -1,3 +1,13 @@
+import { Types } from 'mongoose';
+
 export type TTag = {
+  _id?: Types.ObjectId;
   name: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+};
+
+export type TProductTag = {
+  productId: Types.ObjectId;
+  tagId: Types.ObjectId;
 };
--- backend/src/app/modules/tag/tag.model.ts
+++ backend/src/app/modules/tag/tag.model.ts
@@ -1,13 +1,31 @@
 import { Schema, model } from 'mongoose';
-import { TTag } from './tag.interface';
+import { TProductTag, TTag } from './tag.interface';
 
-const tagSchema = new Schema<TTag>({
-  name: {
-    type: String,
+const tagSchema = new Schema<TTag>(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  { timestamps: true },
+);
+
+const productTagSchema = new Schema<TProductTag>({
+  productId: {
+    type: Schema.Types.ObjectId,
     required: true,
-    unique: true,
+    ref: 'product',
+  },
+  tagId: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: 'tag',
   },
 });
 
+// models
 const Tag = model<TTag>('tag', tagSchema);
+export const ProductTag = model<TProductTag>('productTag', productTagSchema);
 export default Tag;
