41c514e23999e76d25a555e75ed81881360e07a5
imrannaazir <imrannaaziremon@gmail.com>
2024-05-06 19:10:05 +0600

    [ADDED] -  top bar

--- store/src/app/(WithCommonLayout)/page.tsx
+++ store/src/app/(WithCommonLayout)/page.tsx
@@ -4,16 +4,6 @@ const HomePage = () => {
   return (
     <div>
       <AppButton>Hello world</AppButton>
-      <section>
-        <div className="flex flex-wrap justify-center py-20 text-gray-300 my-5">
-          <a
-            href="!"
-            className="button button--aylen px-5 py-3 bg-gray-800 hover:bg-gray-700 hover:text-white relative block focus:outline-none border-2 border-solid rounded-lg text-sm text-center font-semibold uppercase tracking-widest overflow-hidden"
-          >
-            Read More
-          </a>
-        </div>
-      </section>
     </div>
   );
 };
--- store/src/components/ui/AppButton.tsx
+++ store/src/components/ui/AppButton.tsx
@@ -4,7 +4,7 @@ import { cn } from "@/libs/utils";
 import { FaArrowRight } from "react-icons/fa";
 
 const appButtonVariants = cva(
-  "button button--aylen tracking-widest   relative  overflow-hidden  inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "button button--aylen tracking-widest   relative  overflow-hidden  inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-semibold ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
