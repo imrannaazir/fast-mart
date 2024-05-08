78385230552618a3ac4af7c36df2c3a19df43f77
imrannaazir <imrannaaziremon@gmail.com>
2024-05-09 00:11:05 +0600

    [CREATED] - cta in nav

--- store/src/components/navbar/Navbar.tsx
+++ store/src/components/navbar/Navbar.tsx
@@ -1,12 +1,20 @@
+import { FaWhatsapp } from "react-icons/fa";
 import NavCategories from "./NavCategories";
 
 const Navbar = () => {
   return (
-    <div>
+    <div className="flex items-center justify-between">
       {/* Categories */}
       <NavCategories />
       {/* nav links */}
       {/* cta  */}
+      <div className="flex items-center gap-1">
+        <FaWhatsapp size={40} className="text-primary" />
+        <div>
+          <h3 className="text-sm font-semibold">CALL ANYTIME</h3>
+          <h2 className="text-primary font-bold text-lg">280 355 211</h2>
+        </div>
+      </div>
     </div>
   );
 };
