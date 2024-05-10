40ce750c6e9e9c6b09f5181bfb54e0e87caf58e1
imrannaazir <imrannaaziremon@gmail.com>
2024-05-10 11:39:25 +0600

    [ADDED] - footer bottom

--- store/src/components/footer/FooterBottom.tsx
+++ store/src/components/footer/FooterBottom.tsx
@@ -1,8 +1,35 @@
 import assets from "@/assets";
 import Image from "next/image";
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaPinterest,
+  FaTwitter,
+} from "react-icons/fa";
 
 const FooterBottom = () => {
-  const socialLinks = [];
+  const socialLinks = [
+    {
+      id: 1,
+      icon: FaFacebookF,
+      link: "#",
+    },
+    {
+      id: 2,
+      icon: FaTwitter,
+      link: "#",
+    },
+    {
+      id: 3,
+      icon: FaInstagram,
+      link: "#",
+    },
+    {
+      id: 4,
+      icon: FaPinterest,
+      link: "#",
+    },
+  ];
   return (
     <div className="flex justify-between items-center pb-10">
       <p> &copy; 2024 Fastmart All rights reserved</p>
@@ -12,8 +39,15 @@ const FooterBottom = () => {
         height={35}
         alt="secure_payment"
       />
-      <div>
-        <span>Stay connected: </span>
+      <div className="flex items-center gap-2">
+        <span className="text-foreground/80">Stay connected: </span>
+        <div className="flex items-center gap-2">
+          {socialLinks.map((socialLink) => (
+            <a key={socialLink.id} href={socialLink.link}>
+              <socialLink.icon className="text-foreground/80 w-4 h-4 hover:text-primary transition-colors duration-300" />
+            </a>
+          ))}
+        </div>
       </div>
     </div>
   );
