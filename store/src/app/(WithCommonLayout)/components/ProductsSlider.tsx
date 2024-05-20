bfb769feba75ba3c6039f5d9e122df00063ff352
imrannaazir <imrannaaziremon@gmail.com>
2024-05-20 14:23:19 +0600

    [FIXED] - products slider

--- store/src/app/(WithCommonLayout)/components/ProductsSlider.tsx
+++ store/src/app/(WithCommonLayout)/components/ProductsSlider.tsx
@@ -7,19 +7,39 @@ import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 import "../../../styles/slider.css";
+import SwiperCore from "swiper";
 
 // import required modules
 import { Navigation } from "swiper/modules";
 import AppProductCard from "@/components/ui/ProductCard/AppProductCard";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
 
 export default function ProductsSlider() {
-  const prevRef = useRef(null);
-  const nextRef = useRef(null);
+  const prevRef = useRef<HTMLButtonElement>(null);
+  const nextRef = useRef<HTMLButtonElement>(null);
+  const swiperRef = useRef<SwiperCore | null>(null);
+
+  useEffect(() => {
+    if (prevRef.current && nextRef.current && swiperRef.current) {
+      swiperRef.current.navigation.update();
+    }
+  }, []);
+
   return (
     <div className="relative swiper-container ">
       <Swiper
+        onBeforeInit={(swiper) => {
+          swiperRef.current = swiper;
+          if (
+            typeof swiper.params.navigation !== "boolean" &&
+            swiper.params.navigation
+          ) {
+            swiper.params.navigation.prevEl = prevRef.current;
+            swiper.params.navigation.nextEl = nextRef.current;
+          }
+          swiper.navigation.update();
+        }}
         loop={true}
         slidesPerView={4}
         navigation={{
@@ -37,18 +57,18 @@ export default function ProductsSlider() {
           </SwiperSlide>
         ))}
       </Swiper>
-      <div
+      <button
         ref={prevRef}
         className="custom-swiper-button-prev bg-primary text-white p-2 rounded-full"
       >
         <FaAngleLeft />
-      </div>
-      <div
+      </button>
+      <button
         ref={nextRef}
         className="custom-swiper-button-next bg-primary text-white p-2 rounded-full"
       >
         <FaAngleRight />
-      </div>
+      </button>
     </div>
   );
 }
