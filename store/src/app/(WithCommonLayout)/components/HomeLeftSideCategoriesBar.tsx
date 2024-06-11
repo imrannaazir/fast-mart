53c656002f7e03c72d6f3a2249b6cac1cd322d8e
imrannaazir <imrannaaziremon@gmail.com>
2024-06-11 20:39:14 +0600

    [INTEGRATED] - collections in left side bar

--- store/src/app/(WithCommonLayout)/components/HomeLeftSideCategoriesBar.tsx
+++ store/src/app/(WithCommonLayout)/components/HomeLeftSideCategoriesBar.tsx
@@ -1,12 +1,39 @@
-import DropdownCategories from "@/components/navbar/DropdownCategories";
+import DropdownCategories, {
+  TCollectionDropdownItemProps,
+} from "@/components/navbar/DropdownCategories";
 import SidebarSectionHeader from "./SidebarSectionHeader";
 
-const HomeLeftSideCategoriesBar = () => {
+const baseApi = process.env.NEXT_PUBLIC_DB_URL;
+
+const getAllCollections = async () => {
+  const res = await fetch(`${baseApi}/collections`);
+  if (!res.ok) {
+    throw new Error("Failed to fetch categories!");
+  }
+  const data = await res.json();
+  return data?.data;
+};
+
+const HomeLeftSideCategoriesBar = async () => {
+  const collections = await getAllCollections();
+  const collectionsDropdownItems: TCollectionDropdownItemProps[] =
+    collections?.map((collection: any) => ({
+      id: collection?._id,
+      name: collection?.title,
+      iconName: collection?.icon?.name || "ban",
+      categories: collection?.categories?.map((category: any) => ({
+        id: category?._id,
+        name: category?.title,
+      })),
+    }));
   return (
     <div className="p-6 bg-gray-100 rounded-md   space-y-6  sticky top-4">
       <SidebarSectionHeader level="Category" />
       {/* categories */}
-      <DropdownCategories type="simple" />
+      <DropdownCategories
+        type="simple"
+        collections={collectionsDropdownItems}
+      />
     </div>
   );
 };
