ad7cc65213a25c2ea5c2d38a245bc591a9fa2c13
imrannaazir <imrannaaziremon@gmail.com>
2024-09-23 23:51:08 +0600

    ADDED] - search input

--- apps/web/src/app/(CommonLayout)/search/page.tsx
+++ apps/web/src/app/(CommonLayout)/search/page.tsx
@@ -1,15 +1,26 @@
+import AppBreadcrumb, { TAppBreadcrumbItem } from "@/components/ui/AppBreadcrumb";
 import Container from "@/components/ui/Container";
-import HomeSectionLayout from "@/components/ui/HomeSectionLayout";
 import React from "react";
 
-const SearchPage = () => {
+const SearchPage = ({ searchParams }: { searchParams: { q: string } }) => {
+  const searchBreadcrumbItems: TAppBreadcrumbItem[] = [
+    {
+      title: "Search",
+      href: "/search",
+    },
+  ];
+  console.log(searchParams?.q);
+
   return (
-    <Container className="flex">
-      {/* left */}
-      <aside className="w-[236px] bg-red-300"> Side bar</aside>
-      {/* right */}
-      <section className="w-full bg-green-400">Content</section>
-    </Container>
+    <>
+      <AppBreadcrumb title={`Search Results for "${searchParams.q}"`} items={searchBreadcrumbItems} />
+      <Container className="flex">
+        {/* left */}
+        <aside className="w-[236px] bg-red-300"> Side bar</aside>
+        {/* right */}
+        <section className="w-full bg-green-400">Content</section>
+      </Container>
+    </>
   );
 };
 
--- apps/web/src/components/navbar/HeaderSearch.tsx
+++ apps/web/src/components/navbar/HeaderSearch.tsx
@@ -2,20 +2,18 @@
 
 import { Input } from "antd";
 import type { SearchProps } from "antd/es/input/Search";
+import { useRouter } from "next/navigation";
 
 const HeaderSearch = () => {
+  const router = useRouter();
   const { Search } = Input;
-  const onSearch: SearchProps["onSearch"] = (value, _e, info) =>
-    console.log(info?.source, value);
+  const onSearch: SearchProps["onSearch"] = (value, _e, info) => router.push(`/search?q=${encodeURIComponent(value)}`);
+
+  const onChange: SearchProps["onChange"] = (event) => console.log(event.target?.value);
 
   return (
-    <div className="max-w-[550px] w-full hidden lg:block">
-      <Search
-        placeholder="I'm searching for ..."
-        onSearch={onSearch}
-        enterButton
-        size="large"
-      />
+    <div className="hidden w-full max-w-[550px] lg:block">
+      <Search placeholder="I'm searching for ..." onSearch={onSearch} onChange={onChange} enterButton size="large" />
     </div>
   );
 };
