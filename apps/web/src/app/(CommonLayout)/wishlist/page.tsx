2957ff183d5016617f7230f3d1bc7e714b212b9b
imrannaazir <imrannaaziremon@gmail.com>
2024-10-09 21:36:47 +0600

    ADDED: wishlist page

--- apps/web/src/app/(CommonLayout)/wishlist/page.tsx
+++ apps/web/src/app/(CommonLayout)/wishlist/page.tsx
@@ -1,7 +1,31 @@
-import { isAuthenticated } from "@/libs/auth";
+import AppBreadcrumb, { TAppBreadcrumbItem } from "@/components/ui/AppBreadcrumb";
+import Container from "@/components/ui/Container";
+import AppProductCard from "@/components/ui/ProductCard/AppProductCard";
+import { products } from "@/constants/db";
 
 export default function WishlistPage() {
-  const auth = isAuthenticated();
-
-  return <div>WishlistPage</div>;
+  const wishlistBreadcrumbItems: TAppBreadcrumbItem[] = [
+    {
+      title: "Wishlist",
+      href: "/wishlist",
+    },
+  ];
+  return (
+    <>
+      <AppBreadcrumb items={wishlistBreadcrumbItems} title="Wishlist" />
+      <Container className="mb-6 grid grid-cols-5 gap-6">
+        {products.map((product) => (
+          <AppProductCard
+            product={{
+              compare_price: product.compare_price,
+              id: product._id,
+              photo: product?.media?.[0]?.url as string,
+              price: product.price,
+              title: product.title,
+            }}
+          />
+        ))}
+      </Container>
+    </>
+  );
 }
