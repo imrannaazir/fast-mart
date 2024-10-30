cc75e9560d9c72458e1642c833686b8a6bfe8874
imrannaazir <imrannaaziremon@gmail.com>
2024-10-30 23:27:57 +0600

    [fixed] - cart item services

--- apps/api/src/app/modules/cart-item/cart-item.model.ts
+++ apps/api/src/app/modules/cart-item/cart-item.model.ts
@@ -1,29 +1,34 @@
 import { TCartItem } from '@repo/utils/types';
 import { model, Schema } from 'mongoose';
 
-const cartItemSchema = new Schema<TCartItem>({
-  product: {
-    type: String,
-    ref: 'product',
-    as: 'product',
-    required: true,
-  },
-  user: {
-    type: String,
-    ref: 'user',
-    required: true,
-  },
-  options: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'option',
+const cartItemSchema = new Schema<TCartItem>(
+  {
+    product: {
+      type: String,
+      ref: 'product',
+      as: 'product',
+      required: true,
     },
-  ],
-  quantity: {
-    type: Number,
-    required: true,
+    user: {
+      type: String,
+      ref: 'user',
+      required: true,
+    },
+    options: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'option',
+      },
+    ],
+    quantity: {
+      type: Number,
+      required: true,
+    },
   },
-});
+  {
+    timestamps: true,
+  },
+);
 
 const CartItem = model<TCartItem>('cartItem', cartItemSchema);
 export default CartItem;
