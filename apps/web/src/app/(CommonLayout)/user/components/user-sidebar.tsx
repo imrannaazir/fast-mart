65eb3d3f43a7f6917c7d91ab12415b0b53f07755
imrannaazir <imrannaaziremon@gmail.com>
2024-11-08 23:57:06 +0600

    placed: image in the center arroginding to left and bottom

--- apps/web/src/app/(CommonLayout)/user/components/user-sidebar.tsx
+++ apps/web/src/app/(CommonLayout)/user/components/user-sidebar.tsx
@@ -1,5 +1,6 @@
 import assets from "@/assets";
-import { Avatar } from "antd";
+import { Avatar, Divider } from "antd";
+import { Edit } from "lucide-react";
 import Image from "next/image";
 const UserSidebar = () => {
   return (
@@ -15,18 +16,31 @@ const UserSidebar = () => {
 
         {/* avatar */}
         <div className="absolute left-1/2 top-full -translate-x-1/2 -translate-y-1/2 rounded-full bg-gray-100 p-1">
-          <Avatar
-            size={100}
-            src={
-              <img
-                src={"https://img.freepik.com/free-photo/portrait-father-his-backyard_23-2149489567.jpg"}
-                alt="avatar"
-              />
-            }
-          />
+          <div>
+            <Avatar
+              size={100}
+              src={
+                <img
+                  src={"https://img.freepik.com/free-photo/portrait-father-his-backyard_23-2149489567.jpg"}
+                  alt="avatar"
+                />
+              }
+            />
+            <div className="bg-background absolute bottom-2 right-0 rounded-full p-2">
+              <Edit size={20} />
+            </div>
+          </div>
         </div>
       </div>
-      sidebar
+      <div className="p-4">
+        {/* user details  */}
+        <div className="mt-12 text-center">
+          <h2 className="text-[22px] font-semibold">Imran Nazir </h2>
+          <p className="text-sm font-medium text-gray-500">imrannaaziremon@gmail.com</p>
+        </div>
+        <Divider className="" />
+        sidebar
+      </div>
     </aside>
   );
 };
